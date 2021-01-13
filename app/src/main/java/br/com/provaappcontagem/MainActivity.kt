9ef14df753d9da8de3a67410bd6334d64579d4d4
Antenor Manoel <antenor_manuel2010@hotmail.com>
2021-01-13 02:24:02 -0300

    Adicionando funcionalidade dos botões do app

--- app/src/main/java/br/com/provaappcontagem/MainActivity.kt
+++ app/src/main/java/br/com/provaappcontagem/MainActivity.kt
@@ -2,10 +2,27 @@ package br.com.provaappcontagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAdd.setOnClickListener {
+            val valorAtual = (findViewById<TextView>(R.id.contagemTextPlane)).text.toString().toInt()
+            contagemTextPlane.setText((valorAtual + 1).toString())
+        }
+
+        buttonSub.setOnClickListener {
+            val valorAtual = (findViewById<TextView>(R.id.contagemTextPlane)).text.toString().toInt()
+            contagemTextPlane.setText((valorAtual - 1).toString())
+        }
+
+        buttonClean.setOnClickListener {
+            contagemTextPlane.setText("0")
+        }
     }
 }
